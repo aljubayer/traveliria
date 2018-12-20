89c63cd424930483c42cda264bc205785195e894
Jubayer <aljubayer@gmail.com>
2018-12-21 01:38:21 +0600

    update

--- header.php
+++ header.php
@@ -47,79 +47,95 @@ defined( 'ABSPATH' ) OR die( esc_html__( 'This script cannot be accessed directl
 	</section>	 -->
 
 <nav class="main-nav" role="navigation">
-   <!-- Mobile menu toggle button (hamburger/x icon) -->
-   <input id="main-menu-state" type="checkbox" />
-   <label class="main-menu-btn" for="main-menu-state">
-   <span class="main-menu-btn-icon"></span> Toggle main menu visibility
-   </label>
-   <h2 class="nav-brand"><a href="#">Brand</a></h2>
-   <!-- Sample menu definition -->
-   <ul id="main-menu" class="sm sm-simple main-menu-primary">
-      <li><a href="#">Home</a></li>
-      <li>
-         <a href="#">About</a>
-         <ul>
-            <li><a href="#">Introduction to SmartMenus jQuery</a></li>
-            <li><a href="#">Demos + themes</a></li>
-            <li><a href="#">The author</a></li>
+   <div>
+      <!-- Mobile menu toggle button (hamburger/x icon) -->
+      <div class="brand-wrapper">
+         <input id="main-menu-state" type="checkbox" />
+         <label class="main-menu-btn" for="main-menu-state">
+         <span class="main-menu-btn-icon"></span> Toggle main menu visibility
+         </label>
+         <h2 class="nav-brand">
+            <a href="#">Brand</a>
+         </h2>
+      </div>
+      <!-- Sample menu definition -->
+      
+      <div class="menu-wrapper">
+        
+         <ul class="sm sm-simple main-menu-primary">
+            <li> <a href="#">Page 1</a></li>
+            <li> <a href="#">Page 2</a></li>
+         </ul>
+
+         <ul id="main-menu" class="sm sm-simple main-menu-primary">
+            <li><a href="#">Home</a></li>
             <li>
-               <a href="#">The company</a>
+               <a href="#">About</a>
                <ul>
-                  <li><a href="#">About Vadikom</a></li>
-                  <li><a href="#">Projects</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">Privacy policy</a></li>
+                  <li><a href="#">Introduction to SmartMenus jQuery</a></li>
+                  <li><a href="#">Demos + themes</a></li>
+                  <li><a href="#">The author</a></li>
+                  <li>
+                     <a href="#">The company</a>
+                     <ul>
+                        <li><a href="#">About Vadikom</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                     </ul>
+                  </li>
                </ul>
             </li>
-         </ul>
-      </li>
-      <li><a href="#">Download</a></li>
-      <li>
-         <a href="#">Support</a>
-         <ul>
-            <li><a href="#">Premium support</a></li>
-            <li><a href="#">Forums</a></li>
-         </ul>
-      </li>
-      <li><a href="#">Docs</a></li>
-      <li>
-         <a href="#">Sub test</a>
-         <ul>
-            <li><a href="#">Dummy item</a></li>
-            <li><a href="#">Dummy item</a></li>
-            <li><a href="#" class="disabled">Disabled menu item</a></li>
-            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Download</a></li>
             <li>
-               <a href="#">more...</a>
+               <a href="#">Support</a>
                <ul>
-                  <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+                  <li><a href="#">Premium support</a></li>
+                  <li><a href="#">Forums</a></li>
+               </ul>
+            </li>
+            <li><a href="#">Docs</a></li>
+            <li>
+               <a href="#">Sub test</a>
+               <ul>
                   <li><a href="#">Dummy item</a></li>
+                  <li><a href="#">Dummy item</a></li>
+                  <li><a href="#" class="disabled">Disabled menu item</a></li>
                   <li><a href="#">Dummy item</a></li>
                   <li>
                      <a href="#">more...</a>
                      <ul>
+                        <li><a href="#">A pretty long text</a></li>
                         <li><a href="#">Dummy item</a></li>
-                        <li><a href="#" class="current">A 'current' class item</a></li>
                         <li><a href="#">Dummy item</a></li>
                         <li>
                            <a href="#">more...</a>
                            <ul>
-                              <li><a href="#">subMenusMinWidth</a></li>
-                              <li><a href="#">10em</a></li>
-                              <li><a href="#">forced.</a></li>
+                              <li><a href="#">Dummy item</a></li>
+                              <li><a href="#" class="current">A 'current' class item</a></li>
+                              <li><a href="#">Dummy item</a></li>
+                              <li>
+                                 <a href="#">more...</a>
+                                 <ul>
+                                    <li><a href="#">subMenusMinWidth</a></li>
+                                    <li><a href="#">10em</a></li>
+                                    <li><a href="#">forced.</a></li>
+                                 </ul>
+                              </li>
+                              <li><a href="#">Dummy item</a></li>
+                              <li><a href="#">Dummy item</a></li>
                            </ul>
                         </li>
                         <li><a href="#">Dummy item</a></li>
                         <li><a href="#">Dummy item</a></li>
+                        <li><a href="#">Dummy item</a></li>
                      </ul>
                   </li>
-                  <li><a href="#">Dummy item</a></li>
-                  <li><a href="#">Dummy item</a></li>
-                  <li><a href="#">Dummy item</a></li>
                </ul>
             </li>
+            <li><a href="#">Contact</a></li>
          </ul>
-      </li>
-      <li><a href="#">Contact</a></li>
-   </ul>
+      </div>
+   </div>
+   
 </nav>
